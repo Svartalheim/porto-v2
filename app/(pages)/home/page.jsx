602d520ef5187c566fd327bfa73ee7aa200615fc
Svartalheim <gboomers333@gmail.com>
2025-03-17 21:32:33 +0700

    push pc

--- app/(pages)/home/page.jsx
+++ app/(pages)/home/page.jsx
@@ -1,13 +1,13 @@
-import { Wrapper } from '../(components)/wrapper';
-import Bio from './(components)/bio/bio';
-import HeroPage from './(components)/hero/hero';
-import Preface from './(components)/preface/preface';
+import { Wrapper } from '../(components)/wrapper'
+import Bio from './(components)/bio/bio'
+import HeroPage from './(components)/hero/hero'
+import Preface from './(components)/preface/preface'
 
 export default function Home() {
   return (
     <Wrapper
-      theme='dark'
-      className='!design-grid grid-flow-row font-mono'
+      theme="dark"
+      className="!design-grid grid-flow-row font-mono"
       lenis={{ lerp: 0.1, smoothWheel: true }}
     >
       {/* content  */}
@@ -15,5 +15,5 @@ export default function Home() {
       <Preface />
       <Bio />
     </Wrapper>
-  );
+  )
 }
